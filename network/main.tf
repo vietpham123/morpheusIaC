653e827834b74113526242a7608223ad24403fbd
Viet Pham <calijp123@gmail.com>
2022-02-01 09:08:04 -0700

    added network and resourcegroup terraform files

---
+++ network/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+variable "appID" {
+    type = string
+    description = "AppId for Azure"
+    default = ""
+}
+
+variable "client_secret" {
+    type = string
+    description = "Client Secret for Azure"
+    default = ""
+}
+
+variable "tenant_ID" {
+    type = string
+    description = "Tenant ID for Azure"
+    default = ""
+}
+
+variable "subscription_ID" {
+    type = string
+    description = "Subscription ID for Azure"
+    default = ""
+}
+
+variable "azResourceGroupName" {
+   type = string
+   description = "Name of Azure Resource Group"
+}
+
+variable "azRegion" {
+   type = string
+   description = "Azure region to deploy in"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_ID
+  tenant_id = var.tenant_ID
+  client_id = var.appID
+  client_secret = var.client_secret
+}
