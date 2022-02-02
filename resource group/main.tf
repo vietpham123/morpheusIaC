d64e96b850ae7d6c28167cb35fe71241d83c6e90
Viet Pham <calijp123@gmail.com>
2022-02-02 14:21:48 -0700

    updated to remove variables

--- resource group/main.tf
+++ resource group/main.tf
@@ -6,51 +6,12 @@ terraform {
     }
   }
 }
-
-# variable file for creating a resource Group
-
-variable "appID" {
-    type = string
-    description = "AppId for Azure"
-    default = ""
-}
-
-variable "client_secret" {
-    type = string
-    description = "Client Secret for Azure"
-    default = ""
-}
-
-variable "tenant_ID" {
-    type = string
-    description = "Tenant ID for Azure"
-    default = ""
-}
-
-variable "subscription_ID" {
-    type = string
-    description = "Subscription ID for Azure"
-    default = ""
-}
-
-variable "azResourceGroupName" {
-   type = string
-   description = "Name of Azure Resource Group"
-}
-
-variable "azRegion" {
-   type = string
-   description = "Azure region to deploy in"
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_ID
-  tenant_id = var.tenant_ID
-  client_id = var.appID
-  client_secret = var.client_secret
 }
 
 # Create an Azure Resource Group
