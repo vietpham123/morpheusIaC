1a0b6ffb9426b9c2a914fec767ab52da509d17f5
Viet Pham <calijp123@gmail.com>
2022-02-03 07:49:06 -0700

    added azure details

--- resource group/main.tf
+++ resource group/main.tf
@@ -12,6 +12,10 @@ terraform {
 provider "azurerm" {
   features {}
 
+  subscription_id = var.subscription_ID
+  tenant_id = var.tenant_ID
+  client_id = var.appID
+  client_secret = var.client_secret
 }
 
 # Create an Azure Resource Group
