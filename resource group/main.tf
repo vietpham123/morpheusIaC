0449f482099344e9d70e167757c47cbefe786eaf
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2022-02-04 11:21:27 -0600

    Update main.tf

--- resource group/main.tf
+++ resource group/main.tf
@@ -7,6 +7,32 @@ terraform {
   }
 }
 
+variable "azResourceGroupName" {
+    type = string
+    description = "Desired Resource Group Name"
+}
+
+variable "azRegion" {
+    type = string
+    description = "Desired Azure Region"
+}
+
+variable "subscription_ID" {
+    type = string
+}
+
+variable "tenant_ID" {
+    type = string
+}
+
+variable "client_ID" {
+    type = string
+}
+
+variable "client_secret" {
+    type = string
+}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
