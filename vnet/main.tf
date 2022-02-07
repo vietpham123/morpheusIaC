f1bae2cbb66cb6128d2bf8f92a04f97248ecf190
vietpham123 <calijp123@gmail.com>
2022-02-07 15:57:33 -0600

    removed variables

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,29 +1,3 @@
-variable "azResourceGroupName" {
-    type = string
-    description = "Desired Resource Group Name"
-}
-
-variable "azRegion" {
-    type = string
-    description = "Desired Azure Region"
-}
-
-variable "subscription_ID" {
-    type = string
-}
-
-variable "tenant_ID" {
-    type = string
-}
-
-variable "client_ID" {
-    type = string
-}
-
-variable "client_secret" {
-    type = string
-}
-
 # Create virtual network
 resource "azurerm_virtual_network" "hashinet" {
   name                = "vpVnet"
